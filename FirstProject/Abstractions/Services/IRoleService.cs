d31cc7e298e3d9bf77c413596ae501fb4acc72bf
Emin Mammadhuseynov <emin.mov24@gmail.com>
2024-03-02 00:02:47 +0400

    Added Repositories, StudentService, IRoleService

---
+++ FirstProject/Abstractions/Services/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using FirstProject.Models;
+
+namespace FirstProject.Abstractions.Services
+{
+    public interface IRoleService
+    {
+        Task<ResponseModel<object>> GetAllRoles();
+        Task<ResponseModel<object>> GetRoleById(string id);
+        Task<ResponseModel<bool>> CreateRole(string name);
+        Task<ResponseModel<bool>> UpdateRole(string id, string name);
+        Task<ResponseModel<bool>> DeleteRole(string id);
+    }
+}
---
+++ FirstProject/Implementation/Services/RoleService.cs
@@ -0,0 +1,40 @@
+﻿using FirstProject.Abstractions.Services;
+using FirstProject.Models;
+using Microsoft.AspNetCore.Identity;
+using static FirstProject.Entities.Identity.AppUser;
+
+namespace FirstProject.Implementation.Services
+{
+    public class RoleService : IRoleService
+    {
+        readonly RoleManager<AppRole> _roleManager;
+        public RoleService(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public Task<ResponseModel<bool>> CreateRole(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<bool>> DeleteRole(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<object>> GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<object>> GetRoleById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<bool>> UpdateRole(string id, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
